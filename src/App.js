63e6de804b6025f283a942c995b884deb0316ff1
Andrew McInally <realfunnyestate@yahoo.com>
2022-08-15 11:29:59 -0700

    trying to protect routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import PrivateRouter from './components/PrivateRoute'
 
 import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
@@ -18,14 +19,16 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRouter />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
-      <ToastContainer/>
+      <ToastContainer />
     </>
   )
 }
