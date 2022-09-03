826c2a5d81b8e8b7f0388b1b9b29d5601982b5ce
Andrew McInally <realfunnyestate@yahoo.com>
2022-09-03 11:37:33 -0600

    finished private route ; '

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,17 @@
-import {Navigate, Outlet} from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from '../components/Spinner'
 
 const PrivateRoute = () => {
+  const { loggedIn, checkingStatus } = useAuthStatus()
 
-  const loggedIn = false
+  if (checkingStatus) {
+    return <Spinner />
+  }
 
-
-  return 
-    loggedIn ? <Outlet /> : <Navigate to='/sign-in'/>
-  
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />
 }
 
 export default PrivateRoute
+
+
