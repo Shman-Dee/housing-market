f8ac59ee94ff295e8fa69f6b87ec2c4b2164a468
Andrew McInally <realfunnyestate@yahoo.com>
2022-09-05 12:01:38 -0600

    updated the forgot password

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -15,3 +15,8 @@ const PrivateRoute = () => {
 export default PrivateRoute
 
 
+//Dear Aunt Jennifer, Thank you for the amazing gift of gifts.  Nothing will go to waste. Bridget and I are very grateful for your aid on our new exciting adventure. Thank you for including Grandpa.
+
+//Dear Grandma and Grandpa, Thank you for including us on this wonderful tradition. The crib will get endless use and be full of wonderful new experiences. Many pictures to come!
+
+//Dear Aunt Lauren and Uncle Tom, Thank you for the
